adf41df1923dca93fd95b1ca18c3346146b4b362
Frederick-Acquah <Frederickacquah.789@gmail.com>
2022-09-28 12:50:45 -0200

    new files added

--- username-password-tutorial/routes/auth.js
+++ username-password-tutorial/routes/auth.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const passport = require('passport')
-const LocalStrategy = require('passport-local')
-const crypto = require('crypto')
-const db = require('../db')
+const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const crypto = require('crypto');
+const db = require('../db');
 
 const router = express.Router()
 
--- username-password-tutorial/routes/index.js
+++ username-password-tutorial/routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
+var ensureLogIn = require('connect-ensure-login').ensureLoggedIn;
 const request = require('request');
+const db = require('../db');
 
-var router = express.Router()
+var ensureLoggedIn = ensureLogIn();
 
+function fetchTodos(req, res, next) {
+  db.all('SELECT * FROM todos WHERE owner_id = ?', [
+    req.user.id
+  ], function(err, rows) {
+    if (err) { return next(err); }
+    
+    var todos = rows.map(function(row) {
+      return {
+        id: row.id,
+        title: row.title,
+        completed: row.completed == 1 ? true : false,
+        url: '/' + row.id
+      }
+    });
+    res.locals.todos = todos;
+    res.locals.activeCount = todos.filter(function(todo) { return !todo.completed; }).length;
+    res.locals.completedCount = todos.length - res.locals.activeCount;
+    next();
+  });
+}
+
+var router = express.Router();
+
+/* GET home page. */
 router.get('/', (req, res) => {
+  if(!req.user){
+    return res.render('home')
+  } else{
+    res.redirect('/dashboard')
+  }
+})
+
+router.get('/dashboard', (req, res,) => {
     request('https://jsonplaceholder.typicode.com/todos', (error, response, body) => {
       let data = JSON.parse(body)
       res.render('dashboard', { data } )
